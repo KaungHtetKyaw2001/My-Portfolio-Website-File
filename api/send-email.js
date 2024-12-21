b633dde4339d7c4a2a2084214898d9c60efaf17c
KaungHtetKyaw2001 <102726297+KaungHtetKyaw2001@users.noreply.github.com>
2024-12-21 18:00:55 +0700

    Update send-email.js

--- api/send-email.js
+++ api/send-email.js
@@ -1,4 +1,3 @@
-// api/send-email.js
 import nodemailer from 'nodemailer';
 
 export default async (req, res) => {
@@ -6,16 +5,16 @@ export default async (req, res) => {
     const { name, email, message } = req.body;
 
     const transporter = nodemailer.createTransport({
-      service: 'gmail', // You can use any email service here
+      service: 'gmail',
       auth: {
-        user: 'kaunghtetkyaw2001@gmail.com',
-        pass: 'Sumokhk#2001',
+        user: process.env.EMAIL_USER,  // Use environment variable for Gmail
+        pass: process.env.EMAIL_PASS,  // Use environment variable for App Password
       },
     });
 
     const mailOptions = {
       from: email,
-      to: 'kaunghtetkyaw2001@gmail.com', // Your email where you want to receive the messages
+      to: 'kaunghtetkyaw2001@gmail.com',  // Your email where you want to receive messages
       subject: `Message from ${name}`,
       text: message,
     };
@@ -24,10 +23,10 @@ export default async (req, res) => {
       await transporter.sendMail(mailOptions);
       res.status(200).json({ message: 'Message sent successfully!' });
     } catch (error) {
+      console.error(error);  // Log error details
       res.status(500).json({ message: 'Error sending message.' });
     }
   } else {
     res.status(405).json({ message: 'Method Not Allowed' });
   }
 };
-
